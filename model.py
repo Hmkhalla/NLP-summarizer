34e2a9342426dc57cb12a0374bb357c63b1472f7
Hmkhalla <48966000+Hmkhalla@users.noreply.github.com>
2021-11-10 18:37:53 +0100

    input output format for encoder and attention mechanism

--- model.py
+++ model.py
@@ -16,44 +16,49 @@ class EncoderRNN(nn.Module):
         ### TO DO ###
         ### Make it more robust ###
         self.hidden_dim = hidden_dim if hidden_dim !=None else config.hidden_dim
-        self.input_size = input_size if input_size !=None else config.input_size
         self.emb_dim = emb_dim if emb_dim != None else config.emb_dim
         ### END TO DO ###
 
         self.embedding = embedding
-        self.lstm = nn.LSTM(self.emb_dim, self.hidden_dim)
+        self.lstm = nn.LSTM(self.emb_dim, self.hidden_dim, num_layers=1, batch_first=True, bidirectional=True)
 
     def forward(self, input, hidden):
         ''' Perform word embedding and forward rnn
-        :param input: word_id (batch, )
-        :param hidden: encoder hidden states (batch, hidden_dim)
+        :param input: word_id in sequence (batch_size, max_enc_steps)
 
-        :returns output: word_embedding (batch, embedding_dim)
-        :returns hidden : encoder hidden states (batch, hidden_dim)
+        :returns h_enc_seq: hidden encoding states for all sentence (batch_size, max_enc_steps, 2*hidden_dim)
+        :returns hidden : Tuple containing final hidden state & cell state of encoder. Shape of h & c: (batch_size, 2*hidden_dim)
         '''
-        embedded = self.embedding(input).view(1, 1, -1)
-        output = embedded
-        output, hidden = self.lstm(output, hidden)
-        return output, hidden
+        embedded = self.embedding(input)
+        h_enc_seq, hidden = self.lstm(embedded)
+
+        h, c = hidden  # shape of h: 2, bs, n_hid
+        h = torch.cat(list(h), dim=1)  # bs, 2*n_hid
+        c = torch.cat(list(c), dim=1)
+        return h_enc_seq, (h, c)
 
 
 class IntraTemporalAttention(nn.Module):
     def __init__(self):
         super(IntraTemporalAttention, self).__init__()
-        self.W_e = nn.Bilinear(config.hidden_dim, config.hidden_dim, config.attn_dim, bias=False)
+        self.W_e = nn.Bilinear(2*config.hidden_dim, 2*config.hidden_dim, config.attn_dim, bias=False)
 
-    def forward(self, h_enc, h_decoding, enc_padding_mask, sum_exp_att = None):
+    def forward(self, h_d_t, h_enc, enc_padding_mask, sum_exp_att = None):
         ''' Perform INTRA-TEMPORAL ATTENTION ON INPUT SEQUENCE
-        :param h_enc: hidden encoding states per word (batch, n_seq, hidden_dim)
-        :param h_decoding: hidden decoding states per word (batch, n_seq, hidden_dim)
-        :param enc_padding_mask: mask to ignore padded word (empty words for size consideration) (n_batch, n_seq)
-        :param sum_exp_att: temporal sum of attention score (batch, n_seq)
+        :param h_d_t: decoder hidden state at current time step (batch, 2*hidden_dim)
+        :param h_enc: hidden encoding states for all sentence (batch_size, max_enc_steps, 2*hidden_dim)
+        :param enc_padding_mask: Mask for encoder input; Tensor of size (batch_size, max_enc_steps) with values of 0 for pad tokens & 1 for others
+        :param sum_exp_att: summation of attention weights from previous decoder time steps (batch_size, max_enc_steps)
 
-        :returns ct_e: context encoding vector (batch, hidden_dim)
-        :returns alpha_t: normalized attention score (batch, n_seq)
-        :returns sum_exp_att: temporal sum of attention score (batch, n_seq)
+        :returns ct_e: encoder context vector for decoding_step (eq 5 in https://arxiv.org/pdf/1705.04304.pdf) (batch_size, 2*hidden_dim)
+        :returns alphat_e: normalized encoder attention score (batch_size, max_enc_steps)
+        :returns sum_exp_att: actualised summation of attention weights from decoder time steps (batch_size, max_enc_steps)
         '''
-        attn_score = self.W_e(h_decoding, h_enc)
+
+        # attn_score = self.W_e(h_d_t.unsqueeze(1).expand_as(h_enc), h_enc)
+        attn_score = get_cuda(torch.zeros((config.batch_size, config.max_enc_steps), dtype=torch.float))
+        for i in range(config.max_enc_steps):
+            attn_score[i] = self.W_e(h_d_t, h_enc[:, i, :])
 
         exp_att = torch.exp(attn_score)
         if sum_exp_att is None:
@@ -64,41 +69,44 @@ class IntraTemporalAttention(nn.Module):
             sum_exp_att = sum_exp_att + temp
 
         # assign 0 probability for padded elements
-        alpha_t = exp_att * enc_padding_mask
-        normalization_factor = alpha_t.sum(1, keepdim=True)
-        alpha_t = alpha_t / normalization_factor
+        alphat_e = exp_att * enc_padding_mask
+        normalization_factor = alphat_e.sum(1, keepdim=True)
+        alphat_e = alphat_e / normalization_factor
 
-        alpha_t = alpha_t.unsqueeze(1)  # bs,1,n_seq
+        alphat_e = alphat_e.unsqueeze(1)  # bs,1,n_seq
         # Compute encoder context vector
-        ct_e = torch.bmm(alpha_t, h_enc)  # bs, 1, 2*n_hid
+        ct_e = torch.bmm(alphat_e, h_enc)  # bs, 1, 2*n_hid
         ct_e = ct_e.squeeze(1)
-        alpha_t = alpha_t.squeeze(1)
+        alphat_e = alphat_e.squeeze(1)
 
-        return ct_e, alpha_t, sum_exp_att
+        return ct_e, alphat_e, sum_exp_att
 
 class IntraDecoderAttention(nn.Module):
     def __init__(self):
         super(IntraDecoderAttention, self).__init__()
-        self.W_d = nn.Bilinear(config.hidden_dim, config.hidden_dim, config.attn_dim, bias=False)
+        self.W_d = nn.Bilinear(2*config.hidden_dim, 2*config.hidden_dim, config.attn_dim, bias=False)
 
-    def forward(self, h_dec, prev_h_dec):
+    def forward(self, h_d_t, prev_h_dec):
         ''' Perform INTRA-DECODER ATTENTION
-        :param h_dec: hidden encoding states per sentence (batch, hidden_dim)
-        :param prev_h_dec: previous hidden decoding states per sentence (batch, decoding_step, hidden_dim)
+        :param h_d_t: decoder hidden state at current time step (batch_size, 2*hidden_dim)
+        :param prev_h_dec: previous hidden decoding states (batch_size, decoding_step-1, 2*hidden_dim)
 
-        :returns ct_d: context decoding vector (batch, hidden_dim)
-        :returns prev_h_dec: previous hidden decoding states per sentence (batch, decoding_step, hidden_dim)
+        :returns ct_d: decoder context vector for decoding_step (batch_size, 2*hidden_dim)
+        :returns prev_h_dec: previous hidden decoding states (batch_size, decoding_step, 2*hidden_dim)
         '''
         if prev_h_dec is None :
-            prev_h_dec = h_dec.unsqueeze(1)
-            ct_d = get_cuda(torch.zeros(h_dec.size()))
+            prev_h_dec = h_d_t.unsqueeze(1)
+            ct_d = get_cuda(torch.zeros(h_d_t.size()))
         else :
             # TO DO find other way than contigous #
-            attn = self.W_d(h_dec.unsqueeze(1).expand_as(prev_h_dec).contiguous(), prev_h_dec).squeeze(2)
+            #attn_score = self.W_d(h_d_t.unsqueeze(1).expand_as(prev_h_dec).contiguous(), prev_h_dec).squeeze(2)
+            attn_score = get_cuda(torch.zeros(prev_h_dec.size()[:-1], dtype=torch.float))
+            for i in range(attn_score.size()[-1]):
+                attn_score[i] = self.W_e(h_d_t, prev_h_dec[:, i, :])
 
-            alpha_t = F.softmax(attn, dim=1)  # bs, t-1
+            alpha_t = F.softmax(attn_score, dim=1)  # bs, t-1
             ct_d = torch.bmm(alpha_t.unsqueeze(1), prev_h_dec).squeeze(1)  # bs, n_hid
-            prev_h_dec = torch.cat([prev_h_dec, h_dec.unsqueeze(1)], dim=1)  # bs, t, n_hid
+            prev_h_dec = torch.cat([prev_h_dec, h_d_t.unsqueeze(1)], dim=1)  # bs, t, n_hid
 
         return ct_d, prev_h_dec
 
