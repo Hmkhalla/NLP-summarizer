6c7c4493c1ecf55ee6d318456bdb6bd62d3a5d38
Hmkhalla <48966000+Hmkhalla@users.noreply.github.com>
2021-11-24 18:51:10 +0100

    config from paper

--- util/config.py
+++ util/config.py
@@ -6,14 +6,14 @@ vocab_path = 		"data/vocab"
 
 # Hyperparameters
 attn_dim = 1
-hidden_dim = 512
-emb_dim = 256
-batch_size = 200
-max_enc_steps = 55		#99% of the articles are within length 55
-max_dec_steps = 15		#99% of the titles are within length 15
+hidden_dim = 200
+emb_dim = 100
+batch_size = 50
+max_enc_steps = 800		#99% of the articles are within length 55
+max_dec_steps = 100		#99% of the titles are within length 15
 beam_size = 4
 min_dec_steps= 3
-vocab_size = 50000
+vocab_size = 150000
 
 lr = 0.001
 rand_unif_init_mag = 0.02
